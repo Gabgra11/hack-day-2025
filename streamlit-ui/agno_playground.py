896b258223d32793c3874aa2bd61f100c4e80bf6
Owen Hayes <owen.c.hayes@gmail.com>
2025-06-19 23:52:08 -0500

    Delete old code

--- streamlit-ui/agno_playground.py
+++ streamlit-ui/agno_playground.py
@@ -176,11 +176,3 @@ else:
 
 # Footer
 st.divider()
-# st.markdown(
-#     """
-# <div style='text-align: center; color: #666;'>
-#     Built with ❤️ using Agno and Streamlit
-# </div>
-# """,
-#     unsafe_allow_html=True,
-# )
